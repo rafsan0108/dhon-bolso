b076b4b6bfc841fea5232f89da89ccf241ec6a5c
rafsan0108 <rafsanczsian@gmail.com>
2025-02-09 14:56:07 +0600

    dhon amr heda

---
+++ Code.py
@@ -0,0 +1,46 @@
+# Sentence= "Hi I am $ . I have 200$ in my $bank account"
+# a= Sentence.count("$")
+# print(a)
+
+# a= int(input("Your Age: "))
+# Sum= (a % 2)
+# if(Sum>0):
+#     print("Your Age Number Is An Odd Number")
+# else:
+#     print("Your Age Number IS Even")
+
+# a= int(input("Enter First Number:"))
+# b= int(input("Enter Second Number:"))
+# c= int(input("Enter Third Number: "))
+# d= int(input("Enter Fourth Number:"))
+
+# if(a>=b and a>=c and a>=d):
+#     print("The GReatest Number IS: ", a)
+# elif(b>=c and b>=d):
+#      print("The Greatest Number IS:", b)
+# elif(c>=d):
+#     print("The Greatest Number Is:" , c)
+# else:
+#     print("greatest Number is ", d)
+
+# a=5
+# b=3
+# c=1
+# d=9
+# if(a>b and a>c and a>d):
+#     print("greatest number is:", a)
+# elif(d>a):
+#     print("greatest Number Is: ", d)
+
+# a= int(input("Enter A Number: "))
+# sum= (a % 7)
+# if(sum==0):
+#     print("Yes, This number is a multiple of 7")
+# else:
+#     print("No, This number isn't a multiple of 7")
+
+age= int(input("Enter Your Age: "))
+if(age>=18):
+    print("You are old enough to enter")
+else:
+    print("chod khaiya mor khankir chele lifeless kothakar under aged madarchod")
